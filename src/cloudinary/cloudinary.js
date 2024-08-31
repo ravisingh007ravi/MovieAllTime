9269c08df3d7bee7379c14dbda849930d9f0592b
Ravi Singh <ravi6680singh@gmail.com>
2024-08-31 17:16:46 +0530

    Add craete api

---
+++ src/cloudinary/cloudinary.js
@@ -0,0 +1,102 @@
+const cloudinary= require ('cloudinary').v2;
+require('dotenv').config()
+
+exports.UploadImage = async(img) =>{
+try{
+
+    cloudinary.config({ 
+        cloud_name: process.env.CloudName, 
+        api_key: process.env.APIKey, 
+        api_secret: process.env.APISecret 
+    });
+    
+    // Upload an image
+     const uploadResult = await cloudinary.uploader
+       .upload(
+           img, {
+               public_id: 'shoes',
+           }
+       )
+       .catch((error) => {
+           console.log(error);
+       });
+    
+    console.log(uploadResult);
+    
+    // Optimize delivery by resizing and applying auto-format and auto-quality
+    const optimizeUrl = cloudinary.url('shoes', {
+        fetch_format: 'auto',
+        quality: 'auto'
+    });
+    
+    console.log(optimizeUrl);
+    
+    // Transform the image: auto-crop to square aspect_ratio
+    const autoCropUrl = cloudinary.url('shoes', {
+        crop: 'auto',
+        gravity: 'auto',
+        width: 500,
+        height: 500,
+    });
+    
+    console.log(autoCropUrl);    
+}
+catch(e){ return res.status(500).send({status:false, msg:e.Message}) }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { v2 as cloudinary } from 'cloudinary';
+// require('dotenv').config()
+
+// (async function() {
+
+//     cloudinary.config({ 
+//         cloud_name: process.env.CloudName, 
+//         api_key: process.env.APIKey, 
+//         api_secret: process.env.APISecret 
+//     });
+    
+//     // Upload an image
+//      const uploadResult = await cloudinary.uploader
+//        .upload(
+//            'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+//                public_id: 'shoes',
+//            }
+//        )
+//        .catch((error) => {
+//            console.log(error);
+//        });
+    
+//     console.log(uploadResult);
+    
+//     // Optimize delivery by resizing and applying auto-format and auto-quality
+//     const optimizeUrl = cloudinary.url('shoes', {
+//         fetch_format: 'auto',
+//         quality: 'auto'
+//     });
+    
+//     console.log(optimizeUrl);
+    
+//     // Transform the image: auto-crop to square aspect_ratio
+//     const autoCropUrl = cloudinary.url('shoes', {
+//         crop: 'auto',
+//         gravity: 'auto',
+//         width: 500,
+//         height: 500,
+//     });
+    
+//     console.log(autoCropUrl);    
+// })();
---
+++ src/routes/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+const { createUser } = require('../controller/UserControler');
+const { restPassword } = require('../controller/nodemailer')
+
+
+const upload = multer({ storage: multer.diskStorage({}), });
+
+router.post("/register", upload.single('profileImg'), createUser);
+router.post("/restPassword", upload.single(), restPassword);
+
+
+// router.post("/login", logInUserData);
+// router.get("/user/:userId/profile", authentication, authorization, checkUserId, getUserData);
+// router.put("/user/:userId/profile", authentication, authorization, checkUserId, updateUserData);
+
+router.all('/*', (req, res) => {
+    return res.status(404).send({ status: false, msg: "Invalid URL" });
+});
+
+module.exports = router;
