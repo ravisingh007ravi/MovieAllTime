dcc2cf8102c194cecbb5fd1c0d517e695bfefc1d
ravisingh007ravi <ravisingh007ravi@gmail.com>
2024-12-17 17:39:20 +0530

    updated

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,22 @@
-
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const router = require('./routes/routes')
-require('dotenv').config()
+const router = require('./routes/routes');
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-const portId = 5000 || process.env.PORT ;
-
+const portId = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MongooseDB)
-    .then(() => { console.log('MongoDB connected') })
-    .catch((err) => { console.log(err); })
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.log('MongoDB connection error:', err));
 
-app.use('/',router) 
-app.listen(portId, () => { console.log(`Server is running ${portId}`); })
+app.use('/', router);
+
+app.listen(portId, () => {
+    console.log(`Server is running on port ${portId}`);
+});
